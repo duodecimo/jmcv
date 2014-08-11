8d146ba6d009d20169994686f96c2b7ff7165578
duo <duo@localhost>
2014-08-11 17:22:20 -0300

    Exeption for WebcamDetection method on Jmcv.class.

---
+++ jmcv1.0/src/org/duo/jmcv/JmcvWebcamDetectionException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.duo.jmcv;
+
+/**
+ *
+ * @author duo
+ */
+public class JmcvWebcamDetectionException extends Exception {
+
+    public JmcvWebcamDetectionException(String message) {
+        super(message);
+    }
+}
